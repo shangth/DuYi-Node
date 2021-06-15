6eda8619c0b2b48853db6f199cc236eb8cfc2845
商天寒 <1378167382@qq.com>
2021-06-15 18:06:30 +0800

    笔记

--- about os.js
+++ about os.js
@@ -1,15 +1,22 @@
 const os = require("os");
 
+// EOL是获取当前操作系统的换行符(\n  或者   \r\n)
 // console.log(os.EOL);
 
+// 获取CPU架构名  X32 X64
 // console.log(os.arch())
 
-// console.log(os.cpus().length);
+// 获取CPU内核  可以通过length获取内核数量
+// console.log(os.cpus());
 
-// console.log(os.freemem() / 2 ** 30);
+// 获取当前空余内存     除以1024可以转换为KB
+// console.log(os.freemem());
 
-// console.log(os.homedir());
+// 获取用户目录
+// console.log(os.homedir()); // C:\Users\huawei
 
+// 获取主机名
 // console.log(os.hostname());
 
-console.log(os.tmpdir());
+// 以字符串的形式返回操作系统的默认临时文件目录
+// console.log(os.tmpdir());
--- about path.js
+++ about path.js
@@ -1,23 +1,32 @@
 const path = require("path");
 
+// basename返回文件名   第二个参数是文件类型, 匹配到就去掉后缀名, 匹配不上就不去掉
 // const basename = path.basename("fdg/dfgdfg/adfaf/fdgdfgd/a.asf", ".html");
 // console.log(basename);
 
+// 返回当前操作系统分隔符
 // console.log(path.sep);
 
 // console.log(process.env.PATH.split(path.delimiter));
 
+
+// 获取文件的路径名
 // const dir = path.dirname("a/b/c/d");
 // console.log(dir);
 
+// 获取文件后缀名
 // const ext = path.extname("a/b/c/a.js");
 // console.log(ext);
+
+// 获取拼接后的文件名
 // const basePath = "a/b";
 // const fullpath = path.join(basePath, "../", "d.js");
 // console.log(fullpath);
 
+// 返回后面相对于前面的路径
 // const rel = path.relative('/data/orandea/test/aaa', '/data/orandea/impl/bbb');
 // console.log(rel)
 
+// 获取绝对路径
 const absPath = path.resolve(__dirname, "./a.js");
 console.log(absPath);
